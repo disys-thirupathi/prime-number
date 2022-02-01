7bcc0a9f862775ae71e045a1c4d9b0d6fc6b5ce0
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 18:21:40 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter Number");
+		int num = Convert.ToInt32(Console.ReadLine());
+		int temp = 0;
+		for (int i = 1; i < num; i++)
+		{
+			if (num % i == 0)
+				temp++;
+		}
+		if (temp > 1)
+		{
+			Console.WriteLine("Not Prime Number");
+		}
+		else
+		{
+			Console.WriteLine("{0} is a Prime Number", num);
+		}
+	}
+}
